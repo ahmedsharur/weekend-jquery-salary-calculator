94e797a0b878b7d4199acd077fa2b02d5001a10b
ahmedsharur <ahmedshafeysharur@gmail.com>
2021-08-28 13:07:12 -0500

    fixed the employees table

--- salary.js
+++ salary.js
@@ -16,23 +16,16 @@ function addEmployeeSalary(){
     // Target output by id
     let firstName = $('#f-name').val();
     let lastName = $('#l-name').val();
-    let iD = $('#i-d').val();
+    let id = $('#i-d').val();
     let title = $('#title').val();
     let annualSalary = $('#annual-salary').val();
 
     // appending the list in the employee salary calculator
-    $('#employeesalary').append(`<thead>
-    <th>First Name</th>
-    <th>Last Name</th>
-    <th>ID</th>
-    <th>Title</th>
-    <th>Annual Salary</th>
-    <th></th>
-</thead>
+    $('#employeesalary').append(`
 <tr>
      <td>'${firstName}'</td>
     <td>'${lastName}'</td>
-    <td>'${iD}'</td>
+    <td>'${id}'</td>
     <td>'${title}'</td>
     <td>'${annualSalary}'</td>
     <th><button id="delete-button">Delete</button></th>
