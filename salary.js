31417def302637b5bafdb147870e3871a1fc7c14
ahmedsharur <ahmedshafeysharur@gmail.com>
2021-08-28 10:00:54 -0500

    Targeted element by id and also made the fields empty

--- salary.js
+++ salary.js
@@ -10,5 +10,36 @@ function readyNow(){
 }
 
 function addEmployeeSalary(){
-    
+
+    // Target element by id
+    let firstName = $('#f-name').val();
+    let lastName = $('#l-name').val();
+    let iD = $('#i-d').val();
+    let title = $('#title').val();
+    let annualSalary = $('#annual-salary').val();
+
+    $('employeesalary').append(`<thead>
+    <th>First Name</th>
+    <th>Last Name</th>
+    <th>ID</th>
+    <th>Title</th>
+    <th>Annual Salary</th>
+    <th></th>
+</thead>
+<tr>
+    <td>4</td>
+    <td>5</td>
+    <td>4</td>
+    <td>4</td>
+    <td>4</td>
+    <th>Delete</th>
+</tr>`)
+
+    //empty the fields
+    $('#f-name').val('');
+    $('#l-name').val('');
+    $('#i-d').val('');
+    $('#title').val('');
+    $('#annual-salary').val();
+
 }
