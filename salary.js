a8c2e9cf9545e3966c9749368d447959e1d08c05
Prime <academy@Primes-MacBook-Pro.local>
2021-08-28 09:43:03 -0500

    added a submit button and on click function to the salary.js

--- salary.js
+++ salary.js
@@ -1,1 +1,14 @@
 console.log('in salary ')
+
+$(document).ready(readyNow)
+
+
+function readyNow(){
+    console.log('in readyNow')
+
+    $('#submit-button').on('click', addEmployeeSalary)
+}
+
+function addEmployeeSalary(){
+    
+}
